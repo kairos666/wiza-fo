cdfe1765ea35a2d28e228e5bf7641ff90de2d08e
magicD <musashi.miyamoto81@gmail.com>
2020-04-19 22:33:51 +0200

    dev dependencies (profiler pack, debug pack, logger)

--- src/config/bundles.php
+++ src/config/bundles.php
@@ -8,4 +8,7 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
 ];
